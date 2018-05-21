b01cf2290622a0fb1417c5d41400a82e88a50aa6
Maciej Adamiak <adamiak.maciek@gmail.com>
2018-05-21 20:40:51 +0200

    Avoided multiple configuration load

--- src/main/scala/com/madamiak/registry/SchemaRegistryBoot.scala
+++ src/main/scala/com/madamiak/registry/SchemaRegistryBoot.scala
@@ -6,7 +6,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import com.madamiak.registry.dao.DatabaseComponent
 import com.madamiak.registry.service.{HealthService, SchemaRegistryService}
-import com.typesafe.config.ConfigFactory.load
+import com.typesafe.config.{Config, ConfigFactory}
 import slick.jdbc.PostgresProfile.api._
 
 import scala.concurrent.ExecutionContext
@@ -19,12 +19,13 @@ object SchemaRegistryBoot extends App {
   implicit val materializer: ActorMaterializer      = ActorMaterializer()
   implicit val databaseComponent: DatabaseComponent = new DatabaseComponent
 
+  val config: Config                         = ConfigFactory.load()
   val registryService: SchemaRegistryService = new SchemaRegistryService()
   val healthService: HealthService           = new HealthService()
 
   Http().bindAndHandle(registryService.route ~
                        healthService.route,
-                       load().getString("registry.host"),
-                       load().getInt("registry.port"))
+                       config.getString("registry.host"),
+                       config.getInt("registry.port"))
 
 }
